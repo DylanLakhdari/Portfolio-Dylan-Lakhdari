98fa40bfd971bd616b5b7703b39e200991c761a8
DylanLakhdari <emmelf.exe@gmail.com>
2024-11-20 19:08:36 +0100

    Installation de motion + fade animation

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -1,4 +1,4 @@
-//Bootstrap
+//React-bootstrap
 import {Button, Col, Container, Row} from "react-bootstrap";
 
 //Components
@@ -8,13 +8,17 @@ import Footer from "../components/Footer.jsx";
 //React-router-dom
 import {Link} from "react-router-dom";
 
+//Motion
+import { motion } from "motion/react"
+
 function Accueil() {
 
     return (
         <Container className="py-4 pb-0">
             <Header/>
             <main>
-                <div className="p-5 mb-4 bg-body-tertiary rounded-3">
+                {/*Div Dylan Lakhdari*/}
+                <motion.div className="p-5 mb-4 bg-body-tertiary rounded-3" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.2 , duration: 1.8}}>
                     <Container fluid className="py-5">
                         <h1 className="display-5 fw-bold">Dylan Lakhdari</h1>
                         <Col md={8} className="fs-4">
@@ -23,10 +27,11 @@ function Accueil() {
                             iusto labore maxime ratione temporibus unde veritatis, voluptate voluptates?
                         </Col>
                     </Container>
-                </div>
+                </motion.div>
                 <Row className="align-items-md-stretch">
                     <Col md={6}>
-                        <div className="h-100 p-5 text-bg-dark rounded-3">
+                        {/*Div projets*/}
+                        <motion.div className="h-100 p-5 text-bg-dark rounded-3" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.5 , duration: 1.8}}>
                             <h2>Voir mes projets</h2>
                             <p>
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aspernatur
@@ -38,10 +43,11 @@ function Accueil() {
                                     Voir les projets
                                 </Button>
                             </Link>
-                        </div>
+                        </motion.div>
                     </Col>
                     <Col md={6}>
-                        <div className="h-100 p-5 bg-body-tertiary border rounded-3">
+                        {/*Div skills*/}
+                        <motion.div className="h-100 p-5 bg-body-tertiary border rounded-3" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.8 , duration: 1.8}}>
                             <h2>Mes compétences</h2>
                             <p>
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aspernatur
@@ -53,7 +59,7 @@ function Accueil() {
                                     Voir mes compétences
                                 </Button>
                             </Link>
-                        </div>
+                        </motion.div>
                     </Col>
                 </Row>
             </main>
