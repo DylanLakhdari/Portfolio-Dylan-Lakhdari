83bb7e617f721db090aabcd6e575239f4f1879c3
DylanLakhdari <emmelf.exe@gmail.com>
2025-02-26 14:30:37 +0100

    ProjectModal frontend

--- src/components/ProjectModal.jsx
+++ src/components/ProjectModal.jsx
@@ -2,7 +2,10 @@
 import {createContext, useContext, useState} from 'react';
 
 //Bootstrap
-import {Modal, Button, Badge, Container} from "react-bootstrap"
+import {Modal, Button, Badge, Container, Card, Stack} from "react-bootstrap"
+
+//Bootstrap Icons
+import {Github, PlayCircle} from "react-bootstrap-icons";
 
 const ModalContext = createContext();
 const useModal = () => useContext(ModalContext);
@@ -29,17 +32,16 @@ export function ProjectModal() {
             <Modal.Header closeButton>
                 <Modal.Title>{projectData.title}</Modal.Title>
             </Modal.Header>
+            <Card.Img variant="top" src="src/assets/img/card_placeholder.png"/>
             <Container className="mt-4">
                 <Badge variant="primary" className="px-2" href="#">{projectData.tag}</Badge>
             </Container>
             <Modal.Body>{projectData.description}</Modal.Body>
-            <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                    Close
-                </Button>
-                <Button variant="primary" onClick={handleClose}>
-                    Save Changes
-                </Button>
+            <Modal.Footer className="justify-content-center">
+                <Stack gap={2} className="col-md-5 mx-auto">
+                    <Button variant="primary"><PlayCircle className="me-2 mt-1 h4"/>Demo en ligne</Button>
+                    <Button variant="outline-secondary"><Github className="me-2 mt-1 h4"/>Code source</Button>
+                </Stack>
             </Modal.Footer>
         </Modal>
     );
@@ -50,7 +52,7 @@ export function ProjectModalButton() {
 
     return (
         <Button variant="link" className="p-0" onClick={handleShow}>
-            Launch demo modal
+            Voir plus
         </Button>
     );
 }
